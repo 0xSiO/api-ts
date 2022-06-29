8bcbe7a29d68d90d58e7f0a2165ecbdbb4329630
Luc Street <6709262+0xSiO@users.noreply.github.com>
2022-06-29 00:12:43 -0700

    Add some error schemas and responses to OpenAPI definition

--- docs/openapi.ts
+++ docs/openapi.ts
@@ -6,6 +6,61 @@ const apiDefinition: OpenAPIV3.Document = {
     title: 'API',
     version: '0.0.0',
   },
+  components: {
+    schemas: {
+      Error: {
+        type: 'object',
+        required: ['id', 'status', 'message', 'details'],
+        discriminator: { propertyName: 'status' },
+        properties: {
+          id: { type: 'string', format: 'uuid' },
+          status: { type: 'number' },
+          message: { type: 'string' },
+          details: { type: 'object' },
+        },
+      },
+      InternalServerError: {
+        allOf: [
+          { $ref: '#/components/schemas/Error' },
+          {
+            properties: {
+              status: { default: 500 },
+              message: { default: 'An unknown error occurred' },
+            },
+          },
+        ],
+      },
+      BadRequestError: {
+        allOf: [
+          { $ref: '#/components/schemas/Error' },
+          {
+            properties: {
+              status: { default: 400 },
+              message: { default: 'Your request was malformed or invalid' },
+            },
+          },
+        ],
+      },
+    },
+    responses: {
+      InternalServerError: {
+        description: '',
+        content: {
+          'application/json': {
+            schema: { $ref: '#/components/schemas/InternalServerError' },
+          },
+        },
+      },
+      BadRequestError: {
+        description: '',
+        content: {
+          'application/json': {
+            schema: { $ref: '#/components/schemas/BadRequestError' },
+          },
+        },
+      },
+    },
+  },
   paths: {
     '/health': {
       get: {
